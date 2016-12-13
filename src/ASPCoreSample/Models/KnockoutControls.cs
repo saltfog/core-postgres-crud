ba0ab96337a938d8e244c1ee13a091e5951e096a
Casey Jenkins <saltfog@gmail.com>
2016-12-12 22:05:37 -0700

    Knockout controls

---
+++ src/ASPCoreSample/Models/KnockoutControls.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCoreSample.Models
+{
+    public class KnockoutControls
+    {
+        public List<string> OptionValue { get; set; }
+        public string SelectedOptionValue { get; set; }
+        public List<string> MultipleSelectedOptionValues { get; set; }
+        public string RadioSelectedOptionValue { get; set; }
+    }
+}
+
